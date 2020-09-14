d5fa023c04b52d658c8f569959c63786f546f7ea
Jonathan Lin <jlin@winteroftext.ca>
2020-09-14 09:33:28 -0400

    use big sur set desktop
    update script to use big sur desktop script.

--- resources/wallpaper.sh
+++ resources/wallpaper.sh
@@ -64,11 +64,11 @@ elif [[ "$os_version" -eq "10" && "$major_version" -eq "15" ]]; then
         /bin/bash -c "$WALLPAPER_SH" -s "/Users/Shared/$WALLPAPER_FILENAME"
     fi
 elif [[ "$os_version" -eq "11" ]]; then
-    WALLPAPER_SH=$(curl -fsSL https://raw.githubusercontent.com/mozilla/macos-desktop/810e38873c9c4d63b9d4b35cc81c008c88eac1ca/set-desktop-catalina.sh)
-    HASH="a5fd5700616730f3db1af48bf380156a1897197108be359a3c7769b7a359d7c9" # change only after thorough testing
+    WALLPAPER_SH=$(curl -fsSL https://raw.githubusercontent.com/jlin/macos-desktop/master/set-desktop-big-sur.sh)
+    HASH="48dc4ceffcaf775ce743d42fedeff8c28a3a460f3ed298abe074a89ba20893f1" # change only after thorough testing
 
     if [ "$(echo "$WALLPAPER_SH" | shasum -a 256 | awk '{print $1}')" == $HASH ]; then #  if the hashes match then proceed
-        echo "We're on Big Sur so we're going to try to use the Catalina way to set the wallpaper."
+        echo "We're on Big Sur so we're going to try to use the Big Sur way to set the wallpaper."
         /bin/bash -c "$WALLPAPER_SH" -s "/Users/Shared/$WALLPAPER_FILENAME"
     fi       
 else 
