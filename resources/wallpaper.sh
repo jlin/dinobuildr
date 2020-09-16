6967882fe12979fb81f9ae47b1ca965e459c6b91
Jonathan Lin <jlin@winteroftext.ca>
2020-09-15 21:43:11 -0400

    update hash
    update hash for big sur wallpaper script

--- resources/wallpaper.sh
+++ resources/wallpaper.sh
@@ -65,8 +65,9 @@ elif [[ "$os_version" -eq "10" && "$major_version" -eq "15" ]]; then
     fi
 elif [[ "$os_version" -eq "11" ]]; then
     WALLPAPER_SH=$(curl -fsSL https://raw.githubusercontent.com/jlin/macos-desktop/master/set-desktop-big-sur.sh)
-    HASH="48dc4ceffcaf775ce743d42fedeff8c28a3a460f3ed298abe074a89ba20893f1" # change only after thorough testing
-
+    HASH="8857a4962e8c873bb480bd7dba9b8eb73b9d75e5d71804dd8ff72f75894cf048" # change only after thorough testing
+    echo "we're in the right pathway for OS 11"
+    echo "$WALLPAPER_SH" | shasum -a 256 | awk '{print $1}'
     if [ "$(echo "$WALLPAPER_SH" | shasum -a 256 | awk '{print $1}')" == $HASH ]; then #  if the hashes match then proceed
         echo "We're on Big Sur so we're going to try to use the Big Sur way to set the wallpaper."
         /bin/bash -c "$WALLPAPER_SH" -s "/Users/Shared/$WALLPAPER_FILENAME"
